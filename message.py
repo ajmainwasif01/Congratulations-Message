14f14af89b75bd1c74649c0181ce9d6fd184e293
ajmainwasif01 <nipunazmun@gmail.com>
2024-10-01 17:26:42 +0600

    Congratulations message

---
+++ message.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+achievement = input("What did you achieve? ")
+message = "Congratulations " + name + "!" + "\n" + "You've successfully achieved " + achievement + "!" + "\n" + "Keep up the great work!" * 2
+
+print(message)
